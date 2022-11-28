1cc9a674cb916ea03b0b3ae1ef414e7c3ef5a717
sig03 <sig03@naver.com>
2022-11-28 01:52:16 +0000

    testing fetch but error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,22 @@
-export default function Dashboard() {
+'use client';
+
+import { useEffect } from 'react';
+
+async function getDummyJsonData() {
+  const res = await fetch('https://dummyjson.com/products');
+  //return res.json();
+}
+
+export default async function Dashboard() {
+ // const data = await getDummyJsonData();
+
+  // useEffect(() => {
+  //   console.log(data);
+  // },[data]);
+  
   return (
     <>
-      <div>dashboard</div>
+      <div>test</div>
     </>
   )
 }
